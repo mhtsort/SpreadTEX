a1d03e8b760d743fb91ef8a86ecc175c8c870fe8
mhtsort <mhtsort@gmail.com>
2024-06-11 00:25:13 +0300

    jinja_LaTeX first working implementation. TODO test with varius templates and data

--- jinja_LaTeX/jinja_LaTeX.py
+++ jinja_LaTeX/jinja_LaTeX.py
@@ -2,34 +2,18 @@ import jinja2
 from pathlib import Path
 
 
-class TeX_Template(jinja2.Template):
-    '''Objects of this class contain the text of a jinja2 Template of a LaTeX document
-    '''
-    '''DELETE WHEN READY
-    def __init__(self, template, filename:str = "template.tex" ):
-        self.template = jinja2.Template(template)
-        self.filename = filename
-        #Define template parameters
-        # Define your custom delimiters
-        self.template.environment.BLOCK_START_STRING = r"\begin{jinja_block}"
-        self.template.environment.BLOCK_END_STRING = r"\end{jinja_block}"
-        self.template.environment.VARIABLE_START_STRING = r"\begin{jinja}"
-        self.template.environment.VARIABLE_END_STRING = r"\end{jinja}"
-        self.template.environment.COMMENT_START_STRING = r"\begin{jinja_comment}"
-        self.template.environment.COMMENT_END_STRING = r"\end{jinja_comment}"
-        # Create an environment with your custom delimiters
-        self.env = self.template.Environment(
-        variable_start_string = jj.environment.VARIABLE_START_STRING,
-        variable_end_string = jj.environment.VARIABLE_END_STRING
-        )
-        text = r"This is \begin{jinja} myvar1 \end{jinja} and \begin{jinja} myvar2 \end{jinja}"
-        # Create the template using the custom environment
-        template_inside = env.from_string(text)
-    '''
-    def __init__(self,current_directory):
-        # TODO is the current_directory apropriate name?
-        self._environment = jinja2.Environment(loader = jinja2.FileSystemLoader(current_directory))
-        pass
+class TeX_Template():
+
+    def __init__(self, template_folder:Path, template_name:str, environment_variables_dict:dict = None):
+        # TODO is the current_directory appropriate name?
+        self._environment = jinja2.Environment(loader = jinja2.FileSystemLoader(template_folder))
+        # If configuration dictionary is defined, set environment variables.
+        if environment_variables_dict:
+            self.set_environment_variables(environment_variables_dict)
+        # Load a template by name - filename of the template with extension eg. get_template("template.tex")
+        self._template = self._environment.get_template(template_name)
+
+
     def set_environment_variables(self,environment_variables_dict:dict):
         conf = environment_variables_dict
         for conf_variable in conf.keys():
@@ -47,7 +31,20 @@ class TeX_Template(jinja2.Template):
                 case "COMMENT_END_STRING":
                     self._environment.comment_end_string = conf[conf_variable]
                 case _:
-                    print(f"Configuration variable {_} not recognized")
+                    print(f"Configuration variable {conf_variable} not recognized")
+
+    def set_template(self, template_name:str):
+        '''Utility method sets template from the templates available in the environment folder'''
+        self._template = self._environment.get_template(template_name)
+   
+    def load_configuration_file(self, filename:Path)->dict:
+        '''Utility method get configuration from external file'''
+        # TODO Get configuration from external file preferably json
+        configuration_variables_dictionary = {}
+        return configuration_variables_dictionary
+    def render(self,**kwargs) -> str:
+        # TODO needs testing
+        return self._template.render(**kwargs)
     def __str__(self):
         return self.filename
 
@@ -56,22 +53,35 @@ class TeX_Template(jinja2.Template):
 
 
 
-class Data_to_Template():
-    
-    # data defined as a list of tuples. Each tuple contains one row of variables for the template
-    def __init__(self, data:list[tuple] =[], columns_list=[]):
-        self.data = data
-        
+
 if __name__ == '__main__':
-    print("Works fine")
+    print("TESTING")
+    print("At least it runs")
     # Test if file exists
     static_dir = Path(__file__).resolve().parent.parent / 'static'
     print(static_dir)
     # Test if file can be read
     template_file=static_dir / 'template.tex'
-    with open(template_file,"r") as file:
-        for line in file:
-            print(line)
-    # Test if data goes into template
-    var1 = "Jinja Works!"
+
+    test_if_file_exists = False
+    if test_if_file_exists:
+        with open(template_file,"r") as file:
+            for line in file:
+                print(line)
+    # Environment variables
+    conf = dict(
+        BLOCK_START_STRING = r"\begin{jinja_block}",
+        BLOCK_END_STRING =  r"\end{jinja_block}",
+        VARIABLE_START_STRING = r"\begin{jinja}",
+        VARIABLE_END_STRING = r"\end{jinja}",
+        COMMENT_START_STRING = r"\begin{jinja_comment}",
+        COMMENT_END_STRING = r"\end{jinja_comment}",
+    )
+    # Create template object
+    my_template = TeX_Template(static_dir,"template.tex", conf)
+    print("Template created")
+    # Check information
+    print(my_template._environment.variable_start_string)
+    # Render
+    print(my_template.render(var1 = "Successssssss"))
     
